07daccd7422d922a98d89ad859340f252a286345
Trishin Rambajan <161632845+developertrish@users.noreply.github.com>
2025-07-09 22:11:53 +0200

    Update api.php routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompletionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/generate-completion', [CompletionController::class, 'generateCompletion'])
+    ->name('generate.completion')
+    ->middleware('auth:sanctum');
